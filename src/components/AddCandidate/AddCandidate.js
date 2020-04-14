e20d984959194bf21c2e00ccb66491f3980a6a97
Duncan Phelps <phelps.duncan@gmail.com>
2020-04-14 10:19:18 -0500

    the input changes are now saved in the state. and a ADD_CANDIDATE dispatch is sent out on button click.

--- src/components/AddCandidate/AddCandidate.js
+++ src/components/AddCandidate/AddCandidate.js
@@ -12,13 +12,32 @@ import './AddCandidate.css'
 
 
 class AddCandidate extends Component {
-
+    state = {
+      name: '',
+      lawEnforcement: '',
+      parksRec: '',
+      publicWorks: '',
+      firstResponders: '',
+      communityDev: '',
+      administration: '',
+      education: ''
+    }
     handleAdd = () => {
-        console.log("Add candidate");
+        console.log("Add candidate", this.state);
+        let newCandidate = this.state;
+        this.props.dispatch({type: 'ADD_CANDIDATE', payload: newCandidate})
     }
 
     handleCancel = () => {
         console.log("CANCELING");
+    }
+
+    handleChange = (event, typeOf) =>{
+      this.setState({
+        [typeOf]: event.target.value
+      })
+      console.log(this.state);
+
     }
   
     render() {
@@ -27,37 +46,37 @@ class AddCandidate extends Component {
             <h2>Add Candidate</h2>
 
             <label>Name</label>
-            <input placeholder="first and last name"/>
+            <input placeholder="first and last name" onChange={(event)=>this.handleChange(event, 'name')}/>
             <br/>
 
             <h2>Candidate's Proposed Budget</h2>
 
             <label>Law Enforcement</label>
-            <input placeholder="$default"/>
+            <input placeholder="$default" onChange={(event)=>this.handleChange(event, 'lawEnforcement')}/>
             <br/>
 
             <label>Parks/Rec</label>
-            <input placeholder="$default"/>
+            <input placeholder="$default" onChange={(event)=>this.handleChange(event, 'parksRec')}/>
             <br/>
 
             <label>Public Works</label>
-            <input placeholder="$default"/>
+            <input placeholder="$default" onChange={(event)=>this.handleChange(event, 'publicWorks')}/>
             <br/>
 
             <label>First Responders</label>
-            <input placeholder="$default"/>
+            <input placeholder="$default" onChange={(event)=>this.handleChange(event, 'firstResponders')}/>
             <br/>
 
             <label>Community Development</label>
-            <input placeholder="$default"/>
+            <input placeholder="$default" onChange={(event)=>this.handleChange(event, 'communityDev')}/>
             <br/>
 
             <label>Administration</label>
-            <input placeholder="$default"/>
+            <input placeholder="$default" onChange={(event)=>this.handleChange(event, 'administration')}/>
             <br/>
 
             <label>Education</label>
-            <input placeholder="$default"/>
+            <input placeholder="$default" onChange={(event)=>this.handleChange(event, 'education')}/>
             <br/>
 
             <button onClick={this.handleAdd} >Add Candidate</button>
