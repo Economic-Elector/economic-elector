57968ae726e1c9a6bac212d3b2c30a56bd6f9089
joelburdorf <cozlikb@gmail.com>
2020-04-14 10:15:10 -0500

    corrected the election and cadidate sagas to clear error

--- src/redux/sagas/cadidatesSagas.js
+++ src/redux/sagas/cadidatesSagas.js
@@ -3,4 +3,16 @@ import { put, takeLatest } from 'redux-saga/effects';
 
 
 
+
+
+//below is example of a new function and how it's added to the electionsSagas() for export
+function* newFunction(action) {
+
+}
+
+function* cadidatesSagas() {
+    yield takeLatest('above function goes here', newFunction);
+
+}
+
 export default cadidatesSagas;
