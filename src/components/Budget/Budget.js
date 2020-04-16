bdc8acf2cf9d0c717f5884c29f4bb40be11d3302
cminar <colinminar@gmail.com>
2020-04-16 14:51:38 -0500

    before speaker

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -3,38 +3,37 @@
 // This data will be displayed in a bar chart showing the budget breakdown. Once the user has clicked the “Find My Candidate” button, they will be brought to the Results View (3).
 
 
-import React, {Component} from 'react';
-
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './Budget.css'
 import BudgetItem from '../BudgetItem/BudgetItem';
-import {XYPlot, XAxis, YAxis, VerticalGridLines, HorizontalGridLines, VerticalBarSeries, LabelSeries} from 'react-vis';
+import { XYPlot, XAxis, YAxis, VerticalGridLines, HorizontalGridLines, VerticalBarSeries, LabelSeries } from 'react-vis';
 
 class Budget extends Component {
+
     state = {
-      budget: {
-        lawEnforcement: '',
-        parksRec: '',
-        publicWorks: '',
-        firstResponders: '',
-        communityDev: '',
-        administration: '',
-        education: '',
-        total: ''
-      }
+        budget: {
+            lawEnforcement: '',
+            parksRec: '',
+            publicWorks: '',
+            firstResponders: '',
+            communityDev: '',
+            administration: '',
+            education: '',
+            total: ''
+        }
     }
 
-
     componentDidMount = () => {
-      console.log('page mount');
-      console.log('props', this.props)
-      console.log('current election', this.props.reduxState.budget.pastBudget)
+        console.log('page mount');
+        console.log('props', this.props)
+        console.log('current election', this.props.reduxState.budget.pastBudget)
     }
 
     componentDidUpdate = (prevProps) => {
-      if((this.props.reduxState.budget.results !== prevProps.reduxState.budget.results)){
-        this.props.history.push(`/Results`);
-      }
+        if ((this.props.reduxState.budget.results !== prevProps.reduxState.budget.results)) {
+            this.props.history.push(`/Results`);
+        }
     }
 
     handleBack = () => {
@@ -42,118 +41,119 @@ class Budget extends Component {
     }
 
     handleBudgetChange = (event, typeOf) => {
-      this.setState({
-        budget:{
-          ...this.state.budget,
-          [typeOf]: event.target.value
-        }
-      })
-      let tempTotal = parseFloat( parseFloat(this.state.budget.lawEnforcement) + parseFloat(this.state.budget.parksRec) + parseFloat(this.state.budget.publicWorks) + parseFloat(this.state.budget.firstResponders) + parseFloat(this.state.budget.communityDev) + parseFloat(this.state.budget.administration) + parseFloat(this.state.budget.education));
-      this.setState({
-        budget:{
-          ...this.state.budget,
-          total: tempTotal
-        }
-      })
-      console.log(this.state);
+        this.setState({
+            budget: {
+                ...this.state.budget,
+                [typeOf]: event.target.value
+            }
+        })
+        let tempTotal = parseFloat(parseFloat(this.state.budget.lawEnforcement) + parseFloat(this.state.budget.parksRec) + parseFloat(this.state.budget.publicWorks) + parseFloat(this.state.budget.firstResponders) + parseFloat(this.state.budget.communityDev) + parseFloat(this.state.budget.administration) + parseFloat(this.state.budget.education));
+        this.setState({
+            budget: {
+                ...this.state.budget,
+                total: tempTotal
+            }
+        })
+        console.log(this.state);
     }
 
     findCandidate = () => {
-      console.log('STATE IN BUDGET:', this.state.budget)
-      let userBudget = this.state;
-      this.props.dispatch({ type: 'FIND_CANDIDATE', payload: userBudget});
-      this.props.dispatch({ type: 'SET_USER_BUDGET', payload: userBudget});
-      console.log("Finding Candidate Comparing to...", this.props.reduxState);
+        console.log('STATE IN BUDGET:', this.state.budget)
+        let userBudget = this.state;
+        this.props.dispatch({ type: 'FIND_CANDIDATE', payload: userBudget });
+        this.props.dispatch({ type: 'SET_USER_BUDGET', payload: userBudget });
+        console.log("Finding Candidate Comparing to...", this.props.reduxState);
     }
 
-    
 
-  // once server and db is setup needs to be dynamic
+
+    // once server and db is setup needs to be dynamic
     render() {
 
-      // const for bar graphs using react-vis
-      const userData = [{x: 'Law Enforc', y: 10000}, {x: 'Parks/Rec', y: 12000}, {x: 'PublicWorks', y: 50000}, {x: 'First Responders', y: 50000}, {x: 'Community Dev', y: 50000}, {x: 'Administration', y: 50000}, {x: 'Education', y: 50000}];
+        // const for bar graphs using react-vis
+        const userData = [{ x: 'Law Enforc', y: this.state.budget.lawEnforcement }, { x: 'Parks/Rec', y: 12000 }, { x: 'PublicWorks', y: 50000 }, { x: 'First Responders', y: 50000 }, { x: 'Community Dev', y: 50000 }, { x: 'Administration', y: 50000 }, { x: 'Education', y: 50000 }];
 
-      const currentData = [{x: 'Law Enforc', y: 12000}, {x: 'Parks/Rec', y: 14000}, {x: 'PublicWorks', y: 52000}, {x: 'First Responders', y: 52000}, {x: 'Community Dev', y: 51000}, {x: 'Administration', y: 54000}, {x: 'Education', y: 51000}];
+        const currentData = [{ x: 'Law Enforc', y: 12000 }, { x: 'Parks/Rec', y: 14000 }, { x: 'PublicWorks', y: 52000 }, { x: 'First Responders', y: 52000 }, { x: 'Community Dev', y: 51000 }, { x: 'Administration', y: 54000 }, { x: 'Education', y: 51000 }];
 
-      const labelData = userData.map((d, idx) => ({
-        x: d.x,
-        y: Math.max(userData[idx].y, currentData[idx].y)
-      }));
-      const BarSeries = VerticalBarSeries;
-
-
-
-      return (
-          <div className="center_just">
-            
-            <button class="left_just" onclick={this.handleBack}>Back to Elections</button>
-            <div className = 'budgetForm'>
-
-                <h3><u>Create Your Budget Preferences</u></h3>    
-            
-
-              <div class="center_just">
-
-              <label>Law Enforcement</label>
-              <input placeholder="$default" onChange={(event) => this.handleBudgetChange(event, 'lawEnforcement')} />
-              <br />
+        const labelData = userData.map((d, idx) => ({
+            x: d.x,
+            y: Math.max(userData[idx].y, currentData[idx].y)
+        }));
+        const BarSeries = VerticalBarSeries;
 
 
-              <label>Parks/Rec</label>
-              <input placeholder="$default" onChange={(event) => this.handleBudgetChange(event, 'parksRec')} />
-              <br />
 
-              <label>Public Works</label>
-              <input placeholder="$default" onChange={(event) => this.handleBudgetChange(event, 'publicWorks')} />
-              <br />
+        return (
+            <div className="center_just">
 
-              <label>First Responders</label>
-              <input placeholder="$default" onChange={(event) => this.handleBudgetChange(event, 'firstResponders')} />
-              <br />
+                <button class="left_just" onclick={this.handleBack}>Back to Elections</button>
+                <div className='budgetForm'>
 
-              <label>Community Development</label>
-              <input placeholder="$default" onChange={(event) => this.handleBudgetChange(event, 'communityDev')} />
-              <br />
-
-              <label>Administration</label>
-              <input placeholder="$default" onChange={(event) => this.handleBudgetChange(event, 'administration')} />
-              <br />
+                    <h3><u>Create Your Budget Preferences</u></h3>
 
 
-              <label>Education</label>
-              <input placeholder="$default" onChange={(event) => this.handleBudgetChange(event, 'education')} />
-              <br />
+                    <div class="center_just">
 
-              </div>
-              <div class="left_just">
-                <h3><u>Current Budget</u></h3>
-                {this.props.reduxState.budget.pastBudget.map((item) => (<p><BudgetItem item={item} /></p>))}
-              </div>
+                        <label>Law Enforcement</label>
+                        <input placeholder="$default" onChange={(event) => this.handleBudgetChange(event, 'lawEnforcement')} />
+                        <br />
+
+
+                        <label>Parks/Rec</label>
+                        <input placeholder="$default" onChange={(event) => this.handleBudgetChange(event, 'parksRec')} />
+                        <br />
+
+                        <label>Public Works</label>
+                        <input placeholder="$default" onChange={(event) => this.handleBudgetChange(event, 'publicWorks')} />
+                        <br />
+
+                        <label>First Responders</label>
+                        <input placeholder="$default" onChange={(event) => this.handleBudgetChange(event, 'firstResponders')} />
+                        <br />
+
+                        <label>Community Development</label>
+                        <input placeholder="$default" onChange={(event) => this.handleBudgetChange(event, 'communityDev')} />
+                        <br />
+
+                        <label>Administration</label>
+                        <input placeholder="$default" onChange={(event) => this.handleBudgetChange(event, 'administration')} />
+                        <br />
+
+
+                        <label>Education</label>
+                        <input placeholder="$default" onChange={(event) => this.handleBudgetChange(event, 'education')} />
+                        <br />
+
+                    </div>
+                    <div class="left_just">
+                        <h3><u>Current Budget</u></h3>
+                        {this.props.reduxState.budget.pastBudget.map((item) => (<p><BudgetItem item={item} /></p>))}
+                    </div>
+                </div>
+
+                <button class="center_just" onClick={this.findCandidate}>Find My Candidate</button>
+
+                <div class="graph_just">
+                    <XYPlot xType="ordinal" width={1000} height={500} xDistance={700}>
+                        <VerticalGridLines />
+                        <HorizontalGridLines />
+                        <XAxis />
+                        <YAxis />
+                        <BarSeries data={userData} />
+                        <BarSeries data={currentData} />
+                        <LabelSeries data={labelData} />
+                    </XYPlot>
+                </div>
+
+
+
             </div>
+        )
+    }
+}
 
-            <button class="center_just" onClick={this.findCandidate}>Find My Candidate</button>
-
-            <div class="center_just">
-              <XYPlot xType="ordinal" width={1000} height={500} xDistance={700}>
-                <VerticalGridLines />
-                <HorizontalGridLines />
-                <XAxis />
-                <YAxis />
-                <BarSeries data={userData} />
-                <BarSeries data={currentData} />
-                <LabelSeries data={labelData} getLabel={d => d.x}/>
-              </XYPlot>
-            </div>
-            
-
-
-          </div>
-    )}
-  }
-  
-  const mapStateToProps = (reduxState) => ({
+const mapStateToProps = (reduxState) => ({
     reduxState
-  });
-  
-  export default connect(mapStateToProps)(Budget);
+});
+
+export default connect(mapStateToProps)(Budget);
--- src/components/BudgetItem/BudgetItem.js
+++ src/components/BudgetItem/BudgetItem.js
@@ -1,24 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 // Item that displays things about the clicked on election
 // item is the election past budget information from the pastElection reducer
 
 class BudgetItem extends Component {
 
-
     render() {
         console.log('item', this.props.item)
-      return (
-          <div>
-            {this.props.item.name}{this.props.item.past_allocation}
-          </div>
-    )}
-  }
-  
-  const mapStateToProps = (reduxState) => ({
+        return (
+            <div>
+                {this.props.item.name}{this.props.item.past_allocation}
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (reduxState) => ({
     reduxState
-  });
-  
-  export default connect(mapStateToProps)(BudgetItem);
+});
+
+export default connect(mapStateToProps)(BudgetItem);
