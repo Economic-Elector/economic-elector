03a1d40d7f2465293d4792eb5dba637e619611b9
Duncan Phelps <phelps.duncan@gmail.com>
2020-04-26 20:11:18 -0500

    messed with the nav bar. added a home icon. can't decide if it looks good.

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,18 +3,24 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
+import HomeIcon from '@material-ui/icons/Home';
+import { Icon } from '@material-ui/core';
 
 const Nav = (props) => (
     <div className="nav">
         <Link to="/home">
-            <h2 className="nav-title">Economic Elector</h2>
+            <h1 className="nav-title" style={{fontSize: 40}}>Economic Elector</h1>
         </Link>
         <div className="nav-right">
-            <Link className="nav-link" to="/home">
+            <Link className="nav-link" to="/home" style={{display:'flex'}}>
                 {/* Show this link if they are logged in or not,
         but call this link 'Home' if they are logged in,
         and call this link 'Login / Register' if they are not */}
-                {props.user.id ? 'Home' : 'Home'}
+                <div style={{}}>
+                    <HomeIcon style={{ fontSize: 40 }}/>
+                </div>
+                {/* {props.user.id ? 'Home' : 'Home'} */}
+                
             </Link>
             {/* Show the link to the info page and the logout button if the user is logged in */}
             {props.user.id && (
